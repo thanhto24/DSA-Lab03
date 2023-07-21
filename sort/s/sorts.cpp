6e32cde525b1d33a230a82cf6ea89e992eab8f39
khaingovan <khaingovan777@gmail.com>
2023-07-21 09:29:51 +0700

    update heap, merge

--- sort/s/sorts.cpp
+++ sort/s/sorts.cpp
@@ -8,6 +8,149 @@ using namespace std;
 void selectionSort(int arr[], int n, long long &count_compare);
 void selectionSort_noComp(int arr[], int n, double &time_use);
 
+void heapRebuild(int start, int arr[], int n, long long& count_compare){
+	int leftChild = 2 * start + 1; // A left child must exist
+	if (++count_compare && leftChild >= n)return;
+	int largerChild = leftChild; // Make assumption about larger child
+	int rightChild = 2 * start + 2; // A right child might not exist
+	if (++count_compare && rightChild < n){ // Whether a right child exists
+		// A right child exists; check whether it is larger
+		if (++count_compare && arr[rightChild] > arr[largerChild])
+		largerChild = rightChild; // Assumption was wrong
+	}
+	// If arr[start] is smaller than the larger child, swap values
+	if (++count_compare && arr[start] < arr[largerChild]){
+		swap(arr[largerChild], arr[start]);
+		heapRebuild(largerChild, arr, n, count_compare); // Recursion at that child
+	}
+}
+//heapSort count comparison
+void heapSort(int arr[], int n, long long& count_compare){
+	// Build initial heap
+	for (int index = (n - 1) / 2; ++count_compare && index >= 0; index--)
+		heapRebuild(index, arr, n, count_compare);
+	swap(arr[0], arr[n - 1]); // swap the largest element to the end
+	int heapSize = n - 1; // Heap region size decreases by 1
+	while (++count_compare && heapSize > 1) {
+		heapRebuild(0, arr, heapSize, count_compare);
+		heapSize--;
+		swap(arr[0], arr[heapSize]);
+	}
+}
+
+void merge(int arr[], int first, int mid, int last, long long& count_compare){
+	// Initialize the local indices to indicate the subarrays
+	int first1 = first, last1 = mid; // The first subarray
+	int first2 = mid + 1, last2 = last; // The second subarray
+	// Copy the smaller element into the temp array
+	int tempArr[500000]; // Temporary array
+	int index = first1; // Next available location in tempArr
+	while ((++count_compare && first1 <= last1) && (++count_compare && first2 <= last2)) {
+		if (++count_compare && arr[first1] <= arr[first2])
+			tempArr[index++] = arr[first1++];
+		else
+			tempArr[index++] = arr[first2++];
+	}
+	while (++count_compare && first1 <= last1) // Finish the first subarray, if necessary
+		tempArr[index++] = arr[first1++];
+	while (++count_compare && first2 <= last2) // Finish the second subarray, if necessary
+		tempArr[index++] = arr[first2++];
+	// Copy the result back into the original array
+	for (index = first; ++count_compare && index <= last; ++index)
+		arr[index] = tempArr[index];
+}
+
+void mergeSortDo(int arr[], int first, int last, long long& count_compare){
+	if (++count_compare && first < last) {
+		int mid = (first + last) / 2; // Index of midpoint
+			mergeSortDo(arr, first, mid, count_compare); // Sort left half
+			mergeSortDo(arr, mid + 1, last, count_compare); // Sort right half
+			merge(arr, first, mid, last, count_compare); // Merge the two halves
+		}
+}
+//mergeSort count comparison
+void mergeSort(int arr[], int n, long long& count_compare){
+	mergeSortDo(arr, 0, n - 1, count_compare);
+}
+
+void heapRebuild_noComp(int start, int arr[], int n){
+	int leftChild = 2 * start + 1; // A left child must exist
+	if (leftChild >= n) return;
+	int largerChild = leftChild; // Make assumption about larger child
+	int rightChild = 2 * start + 2; // A right child might not exist
+	if (rightChild < n){ // Whether a right child exists
+		// A right child exists; check whether it is larger
+		if (arr[rightChild] > arr[largerChild])
+		largerChild = rightChild; // Assumption was wrong
+	}
+	// If arr[start] is smaller than the larger child, swap values
+	if (arr[start] < arr[largerChild]){
+		swap(arr[largerChild], arr[start]);
+		heapRebuild_noComp(largerChild, arr, n); // Recursion at that child
+	}
+}
+
+void heapSort_noComp(int arr[], int n, double& time){
+	clock_t start, end;
+	start = double(clock());
+	
+	// Build initial heap
+	for (int index = (n - 1) / 2; index >= 0; index--)
+	heapRebuild_noComp(index, arr, n);
+	swap(arr[0], arr[n - 1]); // swap the largest element to the end
+	int heapSize = n - 1; // Heap region size decreases by 1
+	while (heapSize > 1) {
+		heapRebuild_noComp(0, arr, heapSize);
+		heapSize--;
+		swap(arr[0], arr[heapSize]);
+	}
+	
+	end = double(clock());
+	time = (double(end) - double(start)) / double(CLOCKS_PER_SEC);
+}
+
+void merge_noComp(int arr[], int first, int mid, int last){
+	// Initialize the local indices to indicate the subarrays
+	int first1 = first, last1 = mid; // The first subarray
+	int first2 = mid + 1, last2 = last; // The second subarray
+	// Copy the smaller element into the temp array
+	int tempArr[500000]; // Temporary array
+	int index = first1; // Next available location in tempArr
+	while ((first1 <= last1) && (first2 <= last2)) {
+		if (arr[first1] <= arr[first2])
+			tempArr[index++] = arr[first1++];
+		else
+			tempArr[index++] = arr[first2++];
+	}
+	while (first1 <= last1) // Finish the first subarray, if necessary
+	tempArr[index++] = arr[first1++];
+	while (first2 <= last2) // Finish the second subarray, if necessary
+	tempArr[index++] = arr[first2++];
+	// Copy the result back into the original array
+	for (index = first; index <= last; ++index)
+		arr[index] = tempArr[index];
+}
+
+void mergeSortDo_noComp(int arr[], int first, int last){
+	if (first < last) {
+		int mid = (first + last) / 2; // Index of midpoint
+			mergeSortDo_noComp(arr, first, mid); // Sort left half
+			mergeSortDo_noComp(arr, mid + 1, last); // Sort right half
+			merge_noComp(arr, first, mid, last); // Merge the two halves
+		}
+}
+
+void mergeSort_noComp(int arr[], int n, double& time){
+	clock_t start, end;
+	start = double(clock());
+	
+	mergeSortDo_noComp(arr, 0, n - 1);
+	
+	end = double(clock());
+	time = (double(end) - double(start)) / double(CLOCKS_PER_SEC);
+}
+//from Nguyen Ngoc Thao
+
 void radixSort(int arr[], int n, long long &count_compare)
 {
 	// find max value
